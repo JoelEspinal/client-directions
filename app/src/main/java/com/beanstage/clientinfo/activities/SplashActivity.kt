20a85d083c5fcdb17931233f1161345e36d1cccb
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-10 23:51:43 -0400

    ✨ Add five seconds delay transition to clients activity

--- app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
+++ app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.beanstage.clientinfo.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.beanstage.clientinfo.R
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        launcheClientsActivity()
+    }
+
+     fun launcheClientsActivity() {
+        val intent = Intent(this, ClientActivity::class.java)
+        GlobalScope.launch {
+            delay(5000)
+            startActivity(intent)
+            finish()
+        }
     }
 }
