51b2ec7bc4a7ca3069dc1d99e920fa6ff2aea5f1
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-10 22:10:29 -0400

    🚚 Rename splash activity

--- app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
+++ app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.beanstage.clientinfo
+package com.beanstage.clientinfo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beanstage.clientinfo.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
