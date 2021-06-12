699e360b3700046a59b9dd81ee8a87e5141ab8e9
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 16:55:35 -0400

    🔥 Remove unused imports

--- app/src/main/java/com/beanstage/clientinfo/repositories/ClientRepository.kt
+++ app/src/main/java/com/beanstage/clientinfo/repositories/ClientRepository.kt
@@ -1,12 +1,8 @@
 package com.beanstage.clientinfo.repositories
 
-import androidx.activity.viewModels
 import androidx.annotation.WorkerThread
-import com.beanstage.clientinfo.app.ClientApplication
 import com.beanstage.clientinfo.room.daos.ClientDao
 import com.beanstage.clientinfo.room.entities.Client
-import com.beanstage.clientinfo.viewmodels.ClientViewModel
-import com.beanstage.clientinfo.viewmodels.ClientViewModelFactory
 import kotlinx.coroutines.flow.Flow
 
 class ClientRepository(private val clientDao: ClientDao) {
