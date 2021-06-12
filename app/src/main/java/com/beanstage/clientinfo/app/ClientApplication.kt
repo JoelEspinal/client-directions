2184610a74412e9a11377906b4d034eb3be44eb9
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 16:16:33 -0400

    ✨ Add Address repository to app

--- app/src/main/java/com/beanstage/clientinfo/app/ClientApplication.kt
+++ app/src/main/java/com/beanstage/clientinfo/app/ClientApplication.kt
@@ -1,6 +1,7 @@
 package com.beanstage.clientinfo.app
 
 import android.app.Application
+import com.beanstage.clientinfo.repositories.AddressRepository
 import com.beanstage.clientinfo.repositories.ClientRepository
 import com.beanstage.clientinfo.room.ClientRoomDatabase
 import kotlinx.coroutines.CoroutineScope
@@ -11,5 +12,6 @@ class ClientApplication : Application() {
     private val applicationScope = CoroutineScope(SupervisorJob())
 
     val database by lazy { ClientRoomDatabase.getDatabase(this, applicationScope) }
-    val repository by lazy { ClientRepository(database.clientDao()) }
+    val clientRepository by lazy { ClientRepository(database.clientDao()) }
+    val addressRepository by lazy { AddressRepository(database.addressDao()) }
 }
