6048a21e2916ec4073fc14e1e597f67c106e8654
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 08:36:18 -0400

    ✨ Add client list with recycleview

--- app/src/main/java/com/beanstage/clientinfo/activities/ClientActivity.kt
+++ app/src/main/java/com/beanstage/clientinfo/activities/ClientActivity.kt
@@ -2,18 +2,53 @@ package com.beanstage.clientinfo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.beanstage.clientinfo.R
-import com.beanstage.clientinfo.ui.client.ClientFragment
+import com.beanstage.clientinfo.adapters.ClientListAdapter
+import com.beanstage.clientinfo.app.ClientApplication
+import com.beanstage.clientinfo.room.entities.Client
+import com.beanstage.clientinfo.viewmodels.ClientViewModel
 
 class ClientActivity : AppCompatActivity() {
+
+
+    private lateinit var viewModel: ClientViewModel
+
+    private val newWordActivityRequestCode = 1
+//    private val clientViewModel: ClientViewModel by viewModels {
+//        ClientViewModelFactory((application as ClientApplication).repository)
+//    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.client_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, ClientFragment.newInstance())
-                .commitNow()
+//        viewModel = ViewModelProvider(this).get(ClientViewModel::class.java)
+
+        viewModel = ClientViewModel((application as ClientApplication).repository)
+
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.client_recyclerview)
+        val adapter = ClientListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+//
+//        viewModel.allClients().value?.size
+//
+//        var mediatorLiveData: MediatorLiveData<List<Client>> = MediatorLiveData()
+//        mediatorLiveData.addSource(viewModel.allClients(), Observer {
+//            if (it.isNotEmpty()) {
+//                mediatorLiveData.value = it
+//            }
+//        })
+
+        viewModel.allClients.observe(this) { clients ->
+            // Update the cached copy of the words in the adapter.
+            clients.let {  adapter.submitList(clients) }
         }
     }
 }
---
+++ app/src/main/java/com/beanstage/clientinfo/adapters/ClientListAdapter.kt
@@ -0,0 +1,57 @@
+package com.beanstage.clientinfo.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.beanstage.clientinfo.R
+import com.beanstage.clientinfo.room.entities.Client
+
+class ClientListAdapter : ListAdapter<Client, ClientListAdapter.ClientViewHolder>(CLIENT_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientViewHolder {
+        return ClientViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ClientViewHolder, position: Int) {
+        val currentClient = getItem(position)
+        holder.bind(currentClient)
+    }
+
+    class ClientViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val clientNameTextView: TextView = itemView.findViewById(R.id.client_name_textview)
+        private val socialReason: TextView = itemView.findViewById(R.id.social_reason_textview)
+        private val agentNameTextView: TextView = itemView.findViewById(R.id.agent_name_textview)
+
+        fun bind(client: Client?) {
+            clientNameTextView.text = client?.clientName
+            socialReason.text = client?.socialReason
+            agentNameTextView.text = client?.contactAgent
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ClientViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recycleview_client_item, parent, false)
+                return ClientViewHolder(view)
+            }
+        }
+    }
+
+    companion object {
+        private val CLIENT_COMPARATOR = object : DiffUtil.ItemCallback<Client>() {
+            override fun areItemsTheSame(oldClient: Client, newClient: Client): Boolean {
+                return oldClient == newClient
+            }
+
+            override fun areContentsTheSame(oldClient: Client, newClient: Client): Boolean {
+                return (oldClient.clientName == newClient.clientName
+                        && oldClient.socialReason == newClient.socialReason
+                        && oldClient.contactAgent == newClient.contactAgent)
+            }
+        }
+    }
+}
