c5c22583caf3f295190b388a21c0c4b2ad1f805f
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 12:37:39 -0400

    ✨ Add function to create new client

--- app/src/main/java/com/beanstage/clientinfo/activities/ClientFormActivity.kt
+++ app/src/main/java/com/beanstage/clientinfo/activities/ClientFormActivity.kt
@@ -2,33 +2,57 @@ package com.beanstage.clientinfo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.beanstage.clientinfo.R
 import com.beanstage.clientinfo.adapters.ClientListAdapter
 import com.beanstage.clientinfo.app.ClientApplication
+import com.beanstage.clientinfo.room.entities.Client
 import com.beanstage.clientinfo.viewmodels.ClientViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ClientFormActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: ClientViewModel
+    private lateinit var clientViewModel: ClientViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_form)
 
-        viewModel = ClientViewModel((application as ClientApplication).repository)
-
-
+        clientViewModel = ClientViewModel((application as ClientApplication).repository)
 
         val recyclerView = findViewById<RecyclerView>(R.id.address_recyclerview)
         val adapter = ClientListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        viewModel.allClients.observe(this) { clients ->
+        clientViewModel.allClients.observe(this) { clients ->
             // Update the cached copy of the words in the adapter.
             clients.let {  adapter.submitList(clients) }
         }
+
+        val addEditClientButton = findViewById<Button>(R.id.add_edit_client_button)
+        addEditClientButton.setOnClickListener {
+            val newClient = getEditingClient()
+            clientViewModel.insert(newClient)
+        }
+    }
+
+    fun getEditingClient() : Client {
+        val clientName = findViewById<EditText>(R.id.client_name_editText)
+        val clientSocialReason = findViewById<EditText>(R.id.social_reason_editText)
+        val clientContactAgent = findViewById<EditText>(R.id.contact_agent_editText)
+
+        val buisnessName = clientName.text.toString()
+        val socialReason = clientSocialReason.text.toString()
+        val contactAgent =  clientContactAgent.text.toString()
+
+        clientName.text.clear()
+        clientSocialReason.text.clear()
+        clientContactAgent.text.clear()
+
+        return Client(buisnessName, socialReason, contactAgent)
     }
 }
