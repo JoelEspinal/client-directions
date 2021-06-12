e143741502fe7646051526de51969447a17d1a17
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 16:07:07 -0400

    ✨ Add Address entity and dao

---
+++ app/src/main/java/com/beanstage/clientinfo/room/entities/Address.kt
@@ -0,0 +1,15 @@
+package com.beanstage.clientinfo.room.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "address_table")
+data class Address (
+    @PrimaryKey(autoGenerate = true) val addressId: Long?,
+    val clientName: String,
+    @ColumnInfo(name = "sector_name") val sectorName: String,
+    @ColumnInfo(name = "street_name") val streetName: String = "",
+    @ColumnInfo(name = "number") val number: String = "",
+    @ColumnInfo(name = "reference") val reference: String = "",
+)
