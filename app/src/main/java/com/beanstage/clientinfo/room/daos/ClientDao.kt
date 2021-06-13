3a752b645ae0357a66f615c3c223cd97bb3a5745
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-13 17:22:16 -0400

    🚧 Use client id

--- app/src/main/java/com/beanstage/clientinfo/room/daos/ClientDao.kt
+++ app/src/main/java/com/beanstage/clientinfo/room/daos/ClientDao.kt
@@ -12,15 +12,14 @@ interface ClientDao {
     @Query("SELECT * FROM client_table ORDER BY client_name ASC")
     fun getFromClient(): Flow<List<Client>>
 
-    @Query("SELECT * FROM client_table WHERE client_name = :clientName")
-    fun getCurrentClient(clientName: String): Flow<Client>
+    @Query("SELECT * FROM client_table WHERE clientId = :clientId")
+    fun getCurrentClient(clientId: Long): Flow<Client>
 
     @Query("SELECT * FROM client_table")
     fun getAll(): Flow<List<Client>>
 
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(client: Client)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(client: Client): Long
 
     @Query("DELETE FROM client_table")
     suspend fun deleteAll()
