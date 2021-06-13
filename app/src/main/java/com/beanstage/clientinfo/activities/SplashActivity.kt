81f266056d99931000ad529b431701beb201e96f
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-06-12 22:51:03 -0400

    ⚡️ Simplify call new activity

--- app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
+++ app/src/main/java/com/beanstage/clientinfo/activities/SplashActivity.kt
@@ -1,25 +1,14 @@
 package com.beanstage.clientinfo.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.beanstage.clientinfo.R
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        launcheClientsActivity()
-    }
-
-     fun launcheClientsActivity() {
-        val intent = Intent(this, ClientActivity::class.java)
-        runBlocking {
-            delay(5000)
-        }
-
-         startActivity(intent)
-         finish()
+        startActivity(Intent(this@SplashActivity, ClientActivity::class.java))
+        finish()
     }
 }
